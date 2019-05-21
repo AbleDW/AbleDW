f2ba7931215d0a9953030e80c9d2083870c583fe
AbleDW <nanowolfex@gmail.com>
2019-05-21 11:02:30 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='FranciscoBurguete',
-      version='1.0',
-      description='Hello Distribution Module',
-      author='Your Name',
-      author_email='Your Email',
-      url='The URL project - github for example',
-      py_modules=['FranciscoBurguete'],
-     )
